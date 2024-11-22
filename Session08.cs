8441631ca73e94da219283e4a85e2554fdb930b3
giangleueh <giangle.312310262026@st.ueh.edu.vn>
2024-11-22 23:28:12 +0700

    session 0008

--- Session08.cs
+++ Session08.cs
@@ -10,7 +10,100 @@ namespace LeVuNgocGiang_Exercise
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter a string:");
+            string s = Console.ReadLine();
+            Console.WriteLine("You entered: " + s);
+            Console.Write("The length of this string:");
+            Console.WriteLine(Length(s));
+            IndividualChar(s);
+            Console.WriteLine("The string in reverse order: ");
+            ReverseOrder(s);
+            Console.Write("Number of words: ");
+            Console.WriteLine(NumberOfWords(s));
+        }
+            
 
+
+            //to find the length of a string without using a library function
+            static int Length(string s)
+            {
+                int count = 0;
+                foreach (char c in s)
+                {
+                    count++;
+                }
+                return count;
+            }
+
+
+            // to separate individual characters from a string
+            static void IndividualChar(string s)
+            {
+                foreach (char c in s)
+                {
+                    Console.WriteLine(c + " ");
+                }
+            }
+
+            //to print individual characters of the string in reverse order.
+            static void ReverseOrder(string s)
+            {
+          
+                for (int i = s.Length-1; i >=0; i--)
+                {
+                   
+                    Console.WriteLine(s[i] + " ");
+
+                }
+            }
+
+        //to count the total number of words in a string
+        static int NumberOfWords(string s)
+        {
+            int count = 0;
+            int c = ' ';
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] == c) && (s[i + 1] != c))
+                {
+                    count++;
+                }
+            }
+            return count+1;
+        }
+
+
+        //to compare two strings without using a string library functions
+        static void CompareTwoStrings(string s1, string s2)
+        {
+            int l1= s1.Length, l2= s2.Length;
+
+        }
+
+
+
+        //to count the number of alphabets, digits and special characters in a string
+        static void NumberOfAlphabet(string s)
+        {
+            int alpha = 0;
+            int digit = 0;
+            foreach (char c in s)
+            {
+                if (((c > 'a') && (c < 'z')) || ((c > 'A') && (c < 'Z')))
+                {
+                    alpha++;
+                }
+                else if ((c >= '0') && (c<= '9'))
+                {
+                    digit ++;
+                }
+                else
+                {
+
+                }
+            }
+            
         }
     }
 }
